ba7c6baa1935254f9524ad1fb2dcdb6847335b10
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2023-01-06 16:43:46 -0500

    Experience finished

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { experiences } from '../data';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-experience',
@@ -6,5 +14,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
+  experiences = [...experiences];
+
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+
 
 }
